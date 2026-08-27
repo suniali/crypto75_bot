045b6c588e15344408d68e37858589805028f01f
jarvis <alimatroky@gmail.com>
2026-08-27 12:33:17 +0330

    create admin for user alert

--- bot_app/admin.py
+++ bot_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from bot_app.models import UserAlert
+
+
+@admin.register(UserAlert)
+class UserAlertAdmin(admin.ModelAdmin):
+    list_display = ('id','chat_id','symbol','target_price','is_active','created_at')
+    list_filter = ('is_active','symbol','created_at')
+    search_fields = ('chat_id','symbol')
