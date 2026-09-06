ba47d946d428601a4189de6929889e7fc5855e4c
jarvis <alimatroky@gmail.com>
2026-09-06 17:48:12 +0330

    add forex alert to bot

--- bot_app/bot.py
+++ bot_app/bot.py
@@ -1,5 +1,6 @@
 import  os
 import sys
+
 import django
 from pathlib import Path
 from decouple import config
@@ -27,21 +28,26 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_name=update.effective_user.first_name
     await update.message.reply_text(
         f"سلام {user_name} عزیز! به ربات دستیار ترید خوش اومدی.\n\n"
-        f"برای ثبت هشدار قیمت از فرمت زیر استفاده کن:\n"
-        f"/alert BTCUSDT 65000"
+        f"برای ثبت هشدار کریپتو قیمت از فرمت زیر استفاده کن:\n"
+        f"/alert BTCUSDT 65000\n"
+        f"برای ثبت هشدار فارکس قیمت از فرمت زیر استفاده کن:\n"
+        f"/falert XAUUSD 2100 \n"
     )
 
 @sync_to_async
-def save_alert_to_db(chat_id,symbol,target_price):
+def save_alert_to_db(chat_id,symbol,target_price,is_forex):
     is_user_alert_exists=UserAlert.objects.filter(symbol=symbol,target_price=target_price,is_active=True).exists()
 
     if is_user_alert_exists:
         return None
 
+    market_type='FOREX' if is_forex else 'CRYPTO'
     return UserAlert.objects.create(
             chat_id=chat_id,
             symbol=symbol,
-            target_price=target_price)
+            target_price=target_price,
+            market_type=market_type
+    )
 
 async def set_alert(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
@@ -49,17 +55,30 @@ async def set_alert(update: Update, context: ContextTypes.DEFAULT_TYPE):
         symbol = context.args[0].upper()
         target_price = float(context.args[1])
 
-        await save_alert_to_db(chat_id,symbol,target_price)
+        await save_alert_to_db(chat_id,symbol,target_price,False)
 
         await update.message.reply_text(f"✅ هشدار برای {symbol} روی قیمت {target_price} با موفقیت در دیتابیس ثبت شد.")
     except (IndexError, ValueError  ):
         await update.message.reply_text("❌ فرمت اشتباهه! مثال درست:\n/alert BTCUSDT 65000")
+
+async def set_falert(update:Update,context:ContextTypes.DEFAULT_TYPE):
+    try:
+        chat_id = str(update.effective_chat.id)
+        symbol = context.args[0].upper()
+        target_price = float(context.args[1])
+
+        await save_alert_to_db(chat_id, symbol, target_price, True)
+
+        await update.message.reply_text(f"✅ هشدار برای {symbol} روی قیمت {target_price} با موفقیت در دیتابیس ثبت شد.")
+    except (IndexError, ValueError):
+        await update.message.reply_text("❌ فرمت اشتباهه! مثال درست:\n/falert XAUUSD 2100")
 
 if __name__ == '__main__':
     app=ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("alert", set_alert))
+    app.add_handler(CommandHandler('falert',set_falert))
 
     print("ربات روشن شد و آماده دریافت پیام است...")
     app.run_polling()
